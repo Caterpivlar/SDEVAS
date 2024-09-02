4b719a53777f762a747c35ca0d5134933babc8bc
Kate V <vasileva.ekaterina.n@gmail.com>
2024-09-02 16:09:15 +0300

    app.py upd2

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello, World!1'
+
+def hello(name):
+ name='World'
+ return render_template('index.html', name=name)
+
 if __name__ == '__main__':
-	app.run(debug=True)
+ app.run(debug=True)
