356b6251b1b3c693d03a622f4cdf6bca97ca6064
Kate V <vasileva.ekaterina.n@gmail.com>
2024-09-02 17:38:17 +0300

    123

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
+import random
+def new_game():
+    usercount = 0
+    dilercount = 0
+    cards = [6,7,8,9,10,2,3,4,11] * 4
+	random.shuffle(cards)
+def draw_card():
+    currentcard = cards.pop() #тащим последний элемент списка - карту
+    print('Ваша карта', current)
 def test():
 	result='Test OK'
 	return result
