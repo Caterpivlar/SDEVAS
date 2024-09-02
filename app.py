89dcfabce78866cb70188ec642f2e1fce1c7efb5
Kate V <vasileva.ekaterina.n@gmail.com>
2024-09-02 17:49:09 +0300

    1234

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
 @app.route('/')
-import random
-def new_game():
-    usercount = 0
-    dilercount = 0
-    cards = [6,7,8,9,10,2,3,4,11] * 4
-	random.shuffle(cards)
-def draw_card():
-    currentcard = cards.pop() #тащим последний элемент списка - карту
-    print('Ваша карта', current)
+def start():
+	return render_template('index.html',result='Карт нет')
+	
+@app.route('/draw')
 def test():
 	result='Test OK'
-	return result
-
+	return render_template('index.html',result=result)
 if __name__ == '__main__':
 	app.run(debug=True)
