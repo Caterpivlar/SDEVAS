6cee430a8bc5c4b162e9e9954dee26795c09c20c
speechle33 <speechle33@yandex.ru>
2024-09-03 15:51:44 +0300

    сделали маршруты

--- app.py
+++ app.py
@@ -3,16 +3,26 @@ import json
 import requests
 app = Flask(__name__)
 @app.route('/')
+
+def start():
+    return render_template('index.html')
+
+@app.route('/form/')
 def get_deck():
     deck = json.loads(requests.post
 	('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)["deck_id"]
-    return render_template('index.html', deck=deck)
+    return render_template('form.html', deck=deck)
 
-@app.route('/draw/<deck>')
+@app.route('/new_cards/<deck>')
 def draw_card(deck):
     draw = json.loads(requests.get
-	('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count=2').text)["cards"][0]["image"]
-    return render_template('index.html', draw=draw,deck=deck)
-	
+	('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count=1').text)["cards"][0]["image"]
+    return render_template('form.html', draw=draw,deck=deck)
+
+# @app.route('&&&')
+
+
+
+		
 if __name__ == '__main__':
 	app.run(debug=True)
