d25de4d099b84a94182f2d18d7911961e6733be7
speechle33 <speechle33@yandex.ru>
2024-09-04 00:57:40 +0300

    v1.1
    -Пофиксил счёт игрока
    -Добавил кнопку переигры (если счет игрока >21)
    -Сделал неактивной кнопку дай карту (если счет игрока >21)
    -Косметик (мем, текст, теперь с TAB работаем, я привыкаю)

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# табуляция пробелами!!!!!
 from flask import Flask, render_template
 import json
 import requests
@@ -8,34 +7,41 @@ app = Flask(__name__)
 @app.route('/')
 
 def start():
-    cardsrow.clear()
-    score_player.clear()
-    return render_template('index.html')
+	cardsrow.clear()
+	score_player.clear()
+	return render_template('index.html')
 
 @app.route('/form/')
 def get_deck():
-    deck = json.loads(requests.post
+	deck = json.loads(requests.post
 	('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)["deck_id"]
-    return render_template('form.html', deck=deck)
+	cardsrow.clear()
+	score_player.clear()
+	return render_template('form.html', deck=deck)
 
 @app.route('/new_cards/<deck>')
-
 def draw_card(deck):
-    all_info=json.loads(requests.get
+	all_info=json.loads(requests.get
 	('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count=1').text)
-    draw=all_info["cards"][0]["image"]
-    rem =all_info['remaining']
-    value=int(all_info["cards"][0]["value"]) #проблема value= валет, дама...это не int
-	
-#def draw_card(deck): (рабочий код)
-#    draw = json.loads(requests.get
-#	('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count=1').text)["cards"][0]["image"]
-#    rem = json.loads(requests.get
-#    ('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count=0').text)['remaining']
-    score_player.append(value)
-    sum_score_player=int(sum(score_player))
-    cardsrow.append(draw)
-    return render_template('form.html', draw=draw,deck=deck,cardsrow=cardsrow, rem=rem, sum_score_player=sum_score_player)
+	draw=all_info["cards"][0]["image"]
+	rem =all_info['remaining']
+	value=str(all_info["cards"][0]["value"])
+	if value=='JACK' or value=='QUEEN' or value=='KING':
+		value=10
+	elif value=='ACE':
+		value=11	
+	value=int(value)
+	score_player.append(value)
+	sum_score_player=int(sum(score_player))
+	cardsrow.append(draw)
+	if sum_score_player > 21:
+		return render_template('form.html', draw=draw,deck=deck,cardsrow=cardsrow, rem=rem, 
+		sum_score_player=sum_score_player, game_over=True)
+	else:
+		return render_template('form.html', draw=draw,deck=deck,cardsrow=cardsrow, rem=rem, 
+		sum_score_player=sum_score_player, game_over=False)
+		
+
 
 # @app.route('&&&')
 
