dddeba78018102eff408902e128b2a56662b289b
Kate V <vasileva.ekaterina.n@gmail.com>
2024-09-05 13:42:32 +0300

    2 карты
    Тянем 2 карты в начале, а потом по одной

--- app.py
+++ app.py
@@ -14,29 +14,36 @@ def start():
 
 @app.route('/form/')
 def get_deck():
-	deck = json.loads(requests.post
-	('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)["deck_id"]
 	cardsrow.clear()
 	score_player.clear()
 	dealer_cards.clear()
 	score_dealer.clear()
+	deck = json.loads(requests.post
+	('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)["deck_id"]
 	return render_template('form.html', deck=deck)
 
 @app.route('/new_cards/<deck>')
 def draw_card(deck):
-	all_info=json.loads(requests.get #запрос
-	('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count=1').text)
-	draw = all_info["cards"][0]["image"] #картинка карты
-	rem = all_info['remaining'] #сколько карт осталось
-	value = all_info["cards"][0]["value"] #вес карты
-	if value =='JACK' or value == 'QUEEN' or value == 'KING': #условия для валет-туз
-		value = 10
-	elif value == 'ACE':
-		value = 11
-	value = int(value)
-	score_player.append(value) #включение в список значения карты
-	sum_score_player = sum(score_player) #сумма списка
-	cardsrow.insert(0, draw) #включение картинки в список
+	if cardsrow == []: # определение перерменной для вытягивания карт
+		ctd = 2
+	else:
+		ctd = 1
+	i=0
+	all_info = json.loads(requests.get #запрос
+	('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count='+str(ctd)).text)
+	for i in range(ctd): # Цикл для вытягивания 1 или 2 карт в зависимости от необходимости (кол-во ctd)
+		draw = all_info["cards"][i]["image"]   #картинка карты 1
+		rem = all_info['remaining'] #сколько карт осталось
+		value = all_info["cards"][i]["value"] #вес карты
+		if value =='JACK' or value == 'QUEEN' or value == 'KING': #условия для валет-туз
+			value = 10
+		elif value == 'ACE':
+			value = 11
+		value = int(value)
+		score_player.append(value) #включение в список значения карты
+		sum_score_player = sum(score_player) #сумма списка
+		cardsrow.insert(0, draw) #включение картинки в список
+		i=i+1
 	if sum_score_player > 21: #условия проигрыша
 		message = "Ты проиграл, лох"
 		return render_template('form.html', draw=draw, deck=deck, cardsrow=cardsrow, rem=rem, 
@@ -46,9 +53,10 @@ def draw_card(deck):
 		return render_template('form.html', draw=draw,deck=deck,cardsrow=cardsrow, rem=rem, 
 		sum_score_player=sum_score_player, game_over=False)
 
+
 @app.route('/dealer/<deck>')
 def dealer(deck):
-	while sum(score_dealer) <= 17: #!если тянет 2 картинки, то больше 17 и дальше тянет!
+	while sum(score_dealer) <= 17: #!если тянет 2 картинки, то больше 17 и дальше тянет! (у меня все ок)
 		all_info = json.loads(requests.get #запрос
 	('https://deckofcardsapi.com/api/deck/'+deck+'/draw/?count=1').text)
 		draw = all_info["cards"][0]["image"] #картинка карты
